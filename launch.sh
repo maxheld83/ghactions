d02f159c4f95e6bc124b47c1f9b5ed5ea407611b
Maximilian Held <info@maxheld.de>
2019-01-28 16:50:54 +0100

    add package scaffolding

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@ docker run \
   --env=PASSWORD=foo `# some environmental vars` \
   --rm  `# make container ephemeral` \
   -d \
-  --volume=/Users/max/GitHub/ghactions:/home/rstudio \
+  --volume=/Users/max/GitHub/ghactions:/home/rstudio/ghactions \
   --publish=8787:8787  `# port mapping, host left, cont right`\
   --name="ghactions" \
   ghactions `# the image`
