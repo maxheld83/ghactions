b078828802d16a8db9db6ae0a6291b57a3ead6a0
Maximilian Held <info@maxheld.de>
2019-01-30 16:38:41 +0100

    beef up launch

--- launch.sh
+++ launch.sh
@@ -1,4 +1,8 @@
-# I am a comment
+# this step fails when action is not already running, but doesn't matter will continue
+docker stop ghactions
+# this builds the image anew, but this is really fast if there are no changes
+docker build --tag=ghactions .
+# this starts the image and maps the repo directory
 docker run \
   --env=PASSWORD=foo `# some environmental vars` \
   --rm  `# make container ephemeral` \
