99a22377df7379dc088c19d7c11f61b95f1c70b2
Maximilian Held <info@maxheld.de>
2019-02-04 10:13:04 +0100

    enable search, closes #6

---
+++ R/workflows.R
@@ -0,0 +1,35 @@
+use_ghactions <- function() {
+
+  # input validation ===
+
+  # check conditions ===
+
+  # check for RStudio project
+
+  # check for git
+
+  # check for github
+
+  # check for existing main.workflow
+
+  # infer project kind
+
+  # assemble project-specific blocks ===
+
+  use_ghactions_knitr()
+
+
+  # write out main.workflow ===
+}
+
+use_ghactions_knitr <- function() {
+
+  # input validation ===
+
+  # check conditions ===
+
+  # check dockerfile, or offer mainstream dockerfile
+
+
+
+}
