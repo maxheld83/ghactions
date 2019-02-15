4aac8d47b9062c10725eee284e0bb0d219f4c219
Maximilian Held <info@maxheld.de>
2019-02-15 11:40:05 +0100

    reexport functions from usethis as per #76

--- R/setup.R
+++ R/setup.R
@@ -159,3 +159,11 @@ use_ghactions_badge <- function() {
     badge_name = "Actions Status"
   )
 }
+
+#' @importFrom usethis edit_workflow
+#' @export
+usethis::edit_workflow
+
+#' @importFrom usethis browse_github_actions
+#' @export
+usethis::browse_github_actions
