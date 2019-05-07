1de6201a2edf5a98acfece61cd09571f71dcd496
Maximilian Held <info@maxheld.de>
2019-05-07 18:06:40 +0200

    improve comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-# TODO it would be better to do as much as possible of this inside the dockerfile, not the entrypoint
-# For R libraries to persist between github actions (though not across runs) they must be installed somewhere inside $HOME.
-# this sets up R accordingly; all downstream actions using the same container must be set up in this way.
+# For R libraries to persist between github actions (though not across runs) they must be installed somewhere inside $GITHUB_WORKSPACE.
+# hypothesis: $GITHUB_WORKSPACE does not exist at build time of this image, but only at run time, when the github actions runner maps this volume (or whatever).
+# so doing anything in this directory must happen in the entrypoint, not the dockerfile.
+R_LIB_PATH="$GITHUB_WORKSPACE"/lib/R/library # imitating idiomatic path from ~
 # R user library directories must exist before they can be used 
-R_LIB_PATH="$GITHUB_WORKSPACE"/lib/R/library
 mkdir -p "$R_LIB_PATH"
+# this sets up R accordingly; all downstream actions using the same container must be set up in this way.
 echo R_LIBS_USER="$R_LIB_PATH" >> "$HOME"/.Renviron
 
 cat "$HOME"/.Renviron
