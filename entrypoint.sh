e28df97a46bc8d1c3ed895f1da77175f057380a7
Maximilian Held <info@maxheld.de>
2019-05-07 21:02:04 +0200

    poke ghactions, fix docs as per #10

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-# For R libraries to persist between github actions (though not across runs) they must be installed somewhere inside $GITHUB_WORKSPACE.
-# hypothesis: $GITHUB_WORKSPACE does not exist at build time of this image, but only at run time, when the github actions runner maps this volume (or whatever).
+# For R libraries to persist between github actions (though not across runs) they must be installed somewhere inside $HOME.
+# hypothesis: $HOME does not exist at build time of this image, but only at run time, when the github actions runner maps this volume (or whatever).
 # so doing anything in this directory must happen in the entrypoint, not the dockerfile.
 R_LIB_PATH="$HOME"/lib/R/library # imitating idiomatic path from ~
 # R user library directories must exist before they can be used 
