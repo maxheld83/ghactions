6afacd12b93bac2e41612266d459c690c82d5bee
Maximilian Held <info@maxheld.de>
2019-05-07 20:57:43 +0200

    Revert "try with bad path as per https://github.com/maxheld83/ghactions-inst-rdep/issues/10"
    This reverts commit 1a9412bd1f3dc00defaaaee5289a79540de5cc33.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO this is code duplication; would be better to only do this in the upstream container, perhaps by placing .renviron in workspace
-R_LIB_PATH=/githubs/home/lib/R/library
+R_LIB_PATH="$HOME"/lib/R/library
 echo R_LIBS_USER="$R_LIB_PATH" >> "$HOME"/.Renviron
 
 sh -c "Rscript -e '$*'"
