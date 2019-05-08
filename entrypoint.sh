5570d46171b9a988fd53562b9eebd7956beeecf5
Maximilian Held <info@maxheld.de>
2019-05-08 22:05:46 +0200

    make dir only if env provided closes #16

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,11 @@
 
 set -eu
 
-# R user library directories must exist before they can be used 
-mkdir -p "$R_LIBS_USER"
+if [ ! -z "$R_LIBS_USER" ]
+then
+  # R user library directories must exist before they can be used 
+  mkdir -p "$R_LIBS_USER"
+fi
 
 Rscript -e "remotes::install_deps(pkgdir = 'tests/testthat/descriptions/good', upgrade = 'always')"
 
