e210ce2edfb653aacf4ac0a888ca2b1f92648700
Maximilian Held <info@maxheld.de>
2019-05-09 22:40:33 +0200

    add better test case package #9

--- tests/testthat/test-dep-inst.R
+++ tests/testthat/test-dep-inst.R
@@ -1,7 +1,7 @@
 test_that("package from DESCRIPTION is installed", {
   expect_equal(
     object = system2(command = "ls", args = "/github/home/lib/R/library", stdout = TRUE),
-    expected = "glue"
+    expected = "mnormt"
   )
-  expect_silent(object = library(glue))
+  expect_silent(object = library(mnormt))
 })
