2302f3e6b1f287db8d072fe0db8c8a49499a65fe
Maximilian Held <info@maxheld.de>
2019-05-13 19:14:59 +0200

    debug pkg installation #145

--- actions/install-deps/install.R
+++ actions/install-deps/install.R
@@ -1,10 +1,12 @@
 # largely copied from the travis ruby script: https://github.com/travis-ci/travis-build/blob/22a3b1fbdb59170b6c302b2a6a28c8c9cb54b159/lib/travis/build/script/r.rb#L380-L384
 message("Recording already installed dependencies ...")
-deps_exp <- remotes::dev_package_deps(dependencies = TRUE)
+deps_exp <- remotes::dev_package_deps(dependencies = TRUE)$package
 message("Installing dependencies ...")
-remotes::install_deps(dependencies = TRUE)
+remotes::install_deps(dependencies = TRUE, verbose = TRUE)
 message("Checking installation success ...")
-deps_present <- installed.packages(lib.loc = Sys.getenv("R_LIBS_USER"))
+# this includes *all* pkg libs, not just $R_LIBS_USER, because above install_deps will not reinstall pkgs already baked into the image, so we must include those in the test
+deps_present <- installed.packages()[,"Package"]
+# this only compares pkgs, not version numbers or SHAs
 deps_missing <- setdiff(deps_exp, deps_present)
 if (length(deps_missing) == 0) {
   message("All package dependencies were successfully installed.")
