c05048e99433c19d42ab9174b05b3c3531816575
Maximilian Held <info@maxheld.de>
2019-05-22 17:48:55 +0200

    use isolated action library #207 #211

--- actions/install-deps/entrypoint.sh
+++ actions/install-deps/entrypoint.sh
@@ -5,13 +5,6 @@ set -o nounset  # exit on unset vars
 
 echo "Starting dependency installation ..."
 
-if [ ! -z "$R_LIBS_USER" ]
-then
-  # R user library directories must exist before they can be used
-  echo "Creating user library directory at $R_LIBS_USER ..."
-  mkdir -p "$R_LIBS_USER"
-fi
-
 if [ $# -eq 0 ]
   then
     Rscript /install.R
