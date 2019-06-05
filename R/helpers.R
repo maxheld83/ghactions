d883fe5780e95fc7fb5107d301c0da67b5076964
Maximilian Held <info@maxheld.de>
2019-06-05 16:55:37 +0200

    add helper to check for sys deps

--- R/helpers.R
+++ R/helpers.R
@@ -23,3 +23,31 @@ assert_deps <- function(pkgs) {
     call. = FALSE
   )
 }
+
+
+#' Check whether system dependency is available
+#' @noRd
+# TODO use something like this from another package (this is duplicated from pensieve)
+check_sysdep <- function(x) {
+  sys_test <- checkmate::test_character(
+    x = Sys.which(x),
+    min.chars = 2,
+    any.missing = FALSE,
+    all.missing = FALSE,
+    len = 1,
+    null.ok = FALSE
+  )
+  if (sys_test) {
+    return(TRUE)
+  } else {
+    return(
+      glue::glue(
+        "Could not find",
+        x,
+        "system dependency. Try installing it",
+        .sep = " "
+      )
+    )
+  }
+}
+assert_sysdep <- checkmate::makeAssertionFunction(check.fun = check_sysdep)
--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -14,3 +14,14 @@ describe(description = "dependency assertion", code = {
     expect_equivalent(object = assert_deps(good_pkgs), expected = good_pkgs)
   })
 })
+
+describe(description = "system dependency assertion", code = {
+  it("errors out on non-existent system dependency", {
+    bad_sysdep <- "asdasdasdasdasdasd"
+    expect_error(assert_sysdep(x = bad_sysdep))
+  })
+  it("succeds for existing system dependency", {
+    good_sysdep <- "R"
+    expect_equivalent(object = assert_sysdep(good_sysdep), expected = good_sysdep)
+  })
+})
