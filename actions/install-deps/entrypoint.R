9fd8f192de46e35c1d53777d2ebd29ef1ff550ba
Max Held <info@maxheld.de>
2019-06-08 13:41:16 +0200

    remove uneeded pkgs closes #226

--- actions/install-deps/entrypoint.R
+++ actions/install-deps/entrypoint.R
@@ -7,18 +7,26 @@ loadNamespace(package = "curl", lib.loc = Sys.getenv("R_LIBS_ACTION"))
 loadNamespace(package = "git2r", lib.loc = Sys.getenv("R_LIBS_ACTION"))
 loadNamespace(package = "pkgbuild", lib.loc = Sys.getenv("R_LIBS_ACTION"))
 
-message("Recording already installed dependencies ...")
-deps_exp <- remotes::dev_package_deps(dependencies = TRUE)$package
 message("Installing dependencies ...")
 remotes::install_deps(
   dependencies = TRUE,
   verbose = TRUE
 )
+
+message("Removing unneeded dependencies (might come from old cache) ...")
+deps_exp <- remotes::dev_package_deps(dependencies = TRUE)$package
+deps_present <- installed.packages(lib.loc = Sys.getenv("R_LIBS_WORKFLOW"))[, "Package"]
+deps_uneeded <- setdiff(deps_present, deps_exp)
+remove.packages(deps_uneeded)
+message(
+  "Removed one or more package dependencies no longer needed:",
+  paste(deps_uneeded, collapse = ", ")
+)
+
 # TODO ideally, this would be checked by using r-lib/pak or similar inside of install_deps in the above
 # NOTE this is a very incomplete check of success
 # this only compares pkgs, not version numbers or SHAs
-message("Checking installation success ...")
-deps_present <- installed.packages()[, "Package"]
+message("Checking installation success (a little bit)...")
 deps_missing <- setdiff(deps_exp, deps_present)
 if (length(deps_missing) == 0) {
   message("All package dependencies were successfully installed.")
