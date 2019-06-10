f9172d363248b06c611d01b8f03cdec00883ac9c
Max Held <info@maxheld.de>
2019-06-10 10:46:22 +0200

    clean up install-deps #231

--- actions/install-deps/entrypoint.R
+++ actions/install-deps/entrypoint.R
@@ -1,24 +1,30 @@
 #!/usr/bin/env Rscript
+path_action <- Sys.getenv("R_LIBS_ACTION")
+path_workflow <- Sys.getenv("R_LIBS_WORKFLOW")
 
 message("Starting dependency installation ...")
 message("Loading development helper packages from 'R_LIBS_ACTION'.")
-loadNamespace(package = "remotes", lib.loc = Sys.getenv("R_LIBS_ACTION"))
-loadNamespace(package = "curl", lib.loc = Sys.getenv("R_LIBS_ACTION"))
-loadNamespace(package = "git2r", lib.loc = Sys.getenv("R_LIBS_ACTION"))
-loadNamespace(package = "pkgbuild", lib.loc = Sys.getenv("R_LIBS_ACTION"))
+loadNamespace(package = "remotes", lib.loc = path_action)
+loadNamespace(package = "curl", lib.loc = path_action)
+loadNamespace(package = "git2r", lib.loc = path_action)
+loadNamespace(package = "pkgbuild", lib.loc = path_action)
 
 message("Installing dependencies ...")
-remotes::install_deps(
-  dependencies = TRUE,
-  verbose = TRUE
-)
+# this needs to run with modified .libPaths() to recognize cache
+withr::with_libpaths(new = path_workflow, action = "replace", code = {
+  remotes::install_deps(
+    dependencies = TRUE,
+    verbose = TRUE,
+    lib = path_workflow
+  )
+})
 
 message("Checking for unneeded dependencies (might come from old cache) ...")
 deps_exp <- remotes::dev_package_deps(dependencies = TRUE)$package
-deps_present <- installed.packages(lib.loc = Sys.getenv("R_LIBS_WORKFLOW"))[, "Package"]
+deps_present <- installed.packages(lib.loc = path_workflow)[, "Package"]
 deps_uneeded <- setdiff(deps_present, deps_exp)
 if (length(deps_uneeded > 0)) {
-  remove.packages(deps_uneeded)
+  remove.packages(deps_uneeded, lib = path_workflow)
   message(
     "Removed one or more package dependencies no longer needed:",
     paste(deps_uneeded, collapse = ", ")
