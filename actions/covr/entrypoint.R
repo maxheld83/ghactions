cb48053cac27693aaab453dd8c6fd6a36736c45b
Max Held <info@maxheld.de>
2019-06-12 11:55:31 +0200

    also hack-fix covr #247
    this might be same problem as in #247, but I haven't verified that.

--- actions/covr/entrypoint.R
+++ actions/covr/entrypoint.R
@@ -1,5 +1,4 @@
 #!/usr/bin/env Rscript
 
 message("Running test coverage ...")
-loadNamespace(package = "covr", lib.loc = Sys.getenv("R_LIBS_ACTION"))
 covr::codecov(quiet = FALSE, commit = "$GITHUB_SHA", branch = "$GITHUB_REF")
