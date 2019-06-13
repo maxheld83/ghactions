ef9ba52c435e0490e0421d3b4b3cbeba9c151be0
Max Held <info@maxheld.de>
2019-06-13 12:06:17 +0200

    add brew for #253

--- actions/document/entrypoint.R
+++ actions/document/entrypoint.R
@@ -3,4 +3,5 @@
 message("Checking for consistency of roxygen2 with `man` ...")
 loadNamespace(package = "ghactions", lib.loc = Sys.getenv("R_LIBS_ACTION"))
 loadNamespace(package = "roxygen2", lib.loc = Sys.getenv("R_LIBS_ACTION"))
+loadNamespace(package = "brew", lib.loc = Sys.getenv("R_LIBS_ACTION"))
 ghactions::document()
