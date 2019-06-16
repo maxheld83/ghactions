65271fbf6598ad13d357812e74f51009456f23f2
Max Held <info@maxheld.de>
2019-06-16 13:13:26 +0200

    add brew

--- actions/document/entrypoint.R
+++ actions/document/entrypoint.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
 source(file = "/loadNamespace3.R")
+loadNamespace3(package = "brew")
 loadNamespace3(package = "roxygen2")
 loadNamespace3(package = "ghactions")
 message("Checking for consistency of roxygen2 with `man` ...")
