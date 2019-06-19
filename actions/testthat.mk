afa3e135f77261aaa6829926aa77c26e6c2abe9e
Max Held <info@maxheld.de>
2019-06-19 19:34:42 +0200

    migrate old unit tests #260

--- actions/testthat.mk
+++ actions/testthat.mk
@@ -1,12 +1,21 @@
 IMAGE_NAME=$(shell basename $(CURDIR))
+# place *inside* the action container where to conduct tests
+GITHUB_WORKSPACE=/github/workspace
+# TODO would be nice to reuse this here
+R_LIBS_ACTION=/usr/lib/R/dev-helpers-library
 
 .PHONY: test-that
 test-that:
-	# TODO would be nice to use env here
 	docker run \
-	--env="R_LIBS=/usr/lib/R/dev-helpers-library" \
+	--env="R_LIBS=$(R_LIBS_ACTION)" \
 	--entrypoint /usr/bin/Rscript \
+	--volume $(CURDIR):$(GITHUB_WORKSPACE) \
+	--workdir $(GITHUB_WORKSPACE) \
+	--tty \
 	$(IMAGE_NAME) \
 	-e "library(testthat); \
-	    library(checkmate); \
-	    testthat::test_dir(path = '.', stop_on_failure = TRUE)"
+      library(checkmate); \
+      source('/ghactions-source/tests/testthat/helpers.R'); \
+      getwd(); \
+      fs::dir_ls(path = getwd()); \
+      testthat::test_dir(path = '.', stop_on_failure = TRUE)"
