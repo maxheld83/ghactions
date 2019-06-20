43c69e83256094df09285d33d6f30f07a9ffe5f8
Max Held <info@maxheld.de>
2019-06-20 09:35:14 +0200

    no more fancy bind mounts

--- actions/testthat.mk
+++ actions/testthat.mk
@@ -11,13 +11,10 @@ test-that:
 	docker run \
 	--env="R_LIBS=$(R_LIBS_ACTION)" \
 	--entrypoint /usr/bin/Rscript \
-	--mount type=bind,source=$(CURDIR),destination=$(GITHUB_WORKSPACE) \
-	--workdir $(GITHUB_WORKSPACE) \
+	--workdir /ghactions-source/actions/$(IMAGE_NAME) \
 	--tty \
 	$(IMAGE_NAME) \
 	-e "library(testthat); \
       library(checkmate); \
       source('/ghactions-source/tests/testthat/helpers.R'); \
-      getwd(); \
-      fs::dir_ls(path = getwd()); \
       testthat::test_dir(path = '.', stop_on_failure = TRUE)"
