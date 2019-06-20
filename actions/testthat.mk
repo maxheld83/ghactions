1bbf5688db6938fc5d6762072980a7a45fedd502
Max Held <info@maxheld.de>
2019-06-20 10:45:57 +0200

    conditionally map local dir for local make closes #260

--- actions/testthat.mk
+++ actions/testthat.mk
@@ -1,17 +1,31 @@
 IMAGE_NAME=$(shell basename $(CURDIR))
-# place *inside* the action container where to conduct tests
-GITHUB_WORKSPACE=/github/workspace
 # TODO would be nice to reuse this here
 R_LIBS_ACTION=/usr/lib/R/dev-helpers-library
+# place where the source is stored; awkward hack necessary below
+SOURCE_PATH=/ghactions-source/actions
+# always the wd inside the target container
+TEST_PATH=$(SOURCE_PATH)/$(IMAGE_NAME)
+
+# are we inside docker right now?
+ifneq ("$(wildcard /.dockerenv)","")
+	IN_DOCKER=true
+else
+	IN_DOCKER=false
+endif
+
+# weirdly cannot bind mound source:GITHUB_WORKSPACE to anywhere
+# but for local use, we can mount for easier make use
+ifeq ("$(IN_DOCKER)","false")
+	MOUNT_ARG=--mount type=bind,source=$(CURDIR),destination=$(TEST_PATH)
+endif
 
 .PHONY: test-that
 test-that:
-	echo $(CURDIR)
-	ls -a $(CURDIR)
 	docker run \
 	--env="R_LIBS=$(R_LIBS_ACTION)" \
 	--entrypoint /usr/bin/Rscript \
-	--workdir /ghactions-source/actions/$(IMAGE_NAME) \
+	$(MOUNT_ARG) \
+	--workdir $(TEST_PATH) \
 	--tty \
 	$(IMAGE_NAME) \
 	-e "library(testthat); \
