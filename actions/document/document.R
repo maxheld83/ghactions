cbb33e0d74bd0d3574a6c6e219356705b47ee649
Max Held <info@maxheld.de>
2019-06-26 12:52:18 +0000

    accept and document arguments #257

---
+++ actions/document/document.R
@@ -0,0 +1,33 @@
+#!/usr/bin/env Rscript
+
+source_root <- NULL
+# bad hack to make local debugging easier #269
+if (getwd() == "/Users/max/GitHub/ghactions/actions/document") {
+  source_root <- "../../"
+} else if (getwd() != "/Users/max/GitHub/ghactions") {
+  source_root <- "/ghactions-source/"
+
+  source(file = "/loadNamespace3.R")
+  loadNamespace3(package = "brew")
+  loadNamespace3(package = "roxygen2")
+  loadNamespace3(package = "ghactions")
+
+  loadNamespace3(package = "docopt")
+  loadNamespace3(package = "readr")
+}
+
+arguments <- docopt::docopt(
+  doc = readr::read_file(paste0(source_root, "actions/document/man")),
+  help = TRUE,
+  strip_names = TRUE
+)
+
+message("Checking for consistency of roxygen2 with `man` ...")
+
+ghactions::assert_clean_tree(
+  code = {
+    devtools::document()
+  },
+  path = ".",
+  before_code = arguments$before_code
+)
--- actions/document/test_document.R
+++ actions/document/test_document.R
@@ -1,14 +1,14 @@
 context("document")
-
+source_path <- "/ghactions-source/actions/document/"
 test_that(desc = "good docs pass", code = {
   with_blp_repo(
-    code = expect_list(hush(source('/entrypoint.R'))),
+    code = expect_list(hush(source(paste0(source_path, 'document.R')))),
     path = "good"
   )
 })
 test_that(desc = "bad docs fail", code = {
   with_blp_repo(
-    code = expect_error(hush(source('/entrypoint.R'))),
+    code = expect_error(hush(source(paste0(source_path, 'document.R')))),
     path = "bad"
   )
 })
