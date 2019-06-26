65fd0577e715b8567a1d9045b7d318e8b402e516
Max Held <info@maxheld.de>
2019-06-26 15:22:04 +0200

    test argument acceptance closes #257

--- actions/document/test_document.R
+++ actions/document/test_document.R
@@ -1,14 +1,24 @@
 context("document")
-source_path <- "/ghactions-source/actions/document/"
+if (file.exists("/.dockerenv")) {
+  cmd_path <- "/ghactions-source/actions/document/document.R"
+} else {
+  cmd_path <- paste0(getwd(), "/", "document.R")
+}
 test_that(desc = "good docs pass", code = {
   with_blp_repo(
-    code = expect_list(hush(source(paste0(source_path, 'document.R')))),
+    code = expect_equal(
+      object = system2(command = cmd_path, stdout = FALSE, stderr = FALSE),
+      expected = 0L
+    ),
     path = "good"
   )
 })
 test_that(desc = "bad docs fail", code = {
   with_blp_repo(
-    code = expect_error(hush(source(paste0(source_path, 'document.R')))),
+    code = expect_equal(
+      object = system2(command = cmd_path, stderr = FALSE, stdout = FALSE),
+      expected = 1L
+    ),
     path = "bad"
   )
 })
